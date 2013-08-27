760f6b273035152b41214feae979ca31cbb96afb
kevinconan <kevinlyhan@gmail.com>
2013-08-27 20:31:29 +0800

    n

---
+++ ExprCalcCore/ExprCalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExprCalc.Core
+{
+    public class ExprCalc
+    {
+        private  string expr;
+        private string result;
+        public string Result { get; }
+        public string Expr
+        {
+            get;
+            set
+            {
+                expr = value.Trim();
+            }
+        }
+
+
+    }
+}
